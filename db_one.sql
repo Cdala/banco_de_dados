327759d12d4b5318f87b2c7f685a802a8283ebcc
Carlos Dalagrana Assumpção Junior <cdala2007@gmail.com>
2021-01-19 10:06:59 -0300

    Inicial Commit

---
+++ db_one.sql
@@ -0,0 +1,15 @@
+create table cliente
+(codigo numeric (10) not null primary key,
+nome varchar (100) not null,
+telefone varchar (15),
+cidade varchar (30))
+
+
+
+insert into cliente(codigo, nome,telefone,cidade)
+values(1, 'Carlos',41848485699,'Araucaria')
+
+
+
+SELECT codigo, nome, telefone, cidade
+	FROM public.cliente;
